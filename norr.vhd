83526919c4d51144f751bb0c774f0512728eb429
Reno Farnesi <nfarnesi4@gmail.com>
2018-01-17 16:02:26 -0500

    fix nor to be 32 bit input and 1 bit output

--- norr.vhd
+++ norr.vhd
@@ -11,14 +11,16 @@ use ieee.std_logic_1164.all;
 -- Declare entity
 entity norr is
     generic ( n: natural := 32 );
-    port ( x,y: in std_logic_vector(n-1 downto 0);
-           z: out std_logic_vector(n-1 downto 0) );
+    port (x : in std_logic_vector(n-1 downto 0);
+          z: out std_logic);
 end entity;
 
 -- Declare architecture
 architecture dataflow of norr is
+    signal s : std_logic_vector(n downto 0);
 begin
-    gen_not: for i in n-1 downto 0 generate
-        z(i) <= x(i) nor y(i);
+    gen_norr : for i in 0 to n-1 generate
+    	s(i+1) <= x(i) nor s(i);
     end generate;
+    z <= s(n);
 end architecture;
