1a1cc5dac9e804db9ccf0a02965da4ecf2d759fc
Reno Farnesi <nfarnesi4@gmail.com>
2018-01-17 23:04:45 -0500

    mux5: fix mux5 testbench

--- mux5_tb.vhd
+++ mux5_tb.vhd
@@ -1,12 +1,12 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity mux_tb is
-end mux_tb;
+entity mux5_tb is
+end mux5_tb;
 
-architecture behv of mux_tb is
+architecture behv of mux5_tb is
 	component mux
-		generic(n : natural := 5);
+		generic(n : natural := 32);
 		port( x,y: in std_logic_vector(n-1 downto 0);
               sel: in std_logic;
 		      z  : out std_logic_vector(n-1 downto 0));
@@ -17,12 +17,12 @@ architecture behv of mux_tb is
 	signal x, y, z : std_logic_vector(4 downto 0);
 begin
 
-mux_dut : mux port map (x => x, y => y, sel => sel, z => z);
+mux_dut : mux generic map (5) port map (x => x, y => y, sel => sel, z => z);
 
 process
      type pattern_type is record
          --  The inputs of the device.
-	     x   : std_logic_vector(4 downto 0);
+	   x : std_logic_vector(4 downto 0);
          y   : std_logic_vector(4 downto 0);
          sel : std_logic;
          --  The expected outputs of the device.
@@ -31,14 +31,14 @@ process
       --  The patterns to apply.
       type pattern_array is array (natural range <>) of pattern_type;
       constant patterns : pattern_array :=
-         ((X"01", X"00", '0', X"01"),
-          (X"00", X"01", '0', X"00"),
-          (X"01", X"00", '1', X"00"),
-          (X"01", X"01", '1', X"01"),
-          (X"01", X"00", '0', X"01"),
-          (X"00", X"01", '0', X"00"),
-          (X"01", X"00", '1', X"00"),
-          (X"01", X"01", '1', X"01"));
+         (('0' & X"1", '0' & X"0", '0', '0' & X"1"),
+          ('0' & X"0", '0' & X"1", '0', '0' & X"0"),
+          ('0' & X"1", '0' & X"0", '1', '0' & X"0"),
+          ('0' & X"1", '0' & X"1", '1', '0' & X"1"),
+          ('0' & X"1", '0' & X"0", '0', '0' & X"1"),
+          ('0' & X"0", '0' & X"1", '0', '0' & X"0"),
+          ('0' & X"1", '0' & X"0", '1', '0' & X"0"),
+          ('0' & X"1", '0' & X"1", '1', '0' & X"1"));
    begin
       --  Check each pattern.
       for i in patterns'range loop
