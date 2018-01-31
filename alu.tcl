30e464cf65ffb617bf4ada5c085c1fb29bfe9bef
Reno Farnesi <nfarnesi4@gmail.com>
2018-01-31 14:42:13 -0500

    alu: add finilized tcl scripts used in class

--- alu.tcl
+++ alu.tcl
@@ -1,31 +1,63 @@
-add_force {/alu32/a} -radix hex {1 0ns}
-add_force {/alu32/b} -radix hex {3 0ns}
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {9 0ns}
 add_force {/alu32/oper} -radix bin {0000 0ns}
 run 100 ns
 
-add_force {/alu32/a} -radix hex {1 0ns}
-add_force {/alu32/b} -radix hex {3 0ns}
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {9 0ns}
 add_force {/alu32/oper} -radix bin {0001 0ns}
 run 100 ns
 
-add_force {/alu32/a} -radix hex {1 0ns}
-add_force {/alu32/b} -radix hex {3 0ns}
-add_force {/alu32/oper} -radix bin {0010 0ns}
-run 100 ns
-
-add_force {/alu32/a} -radix hex {1 0ns}
-add_force {/alu32/b} -radix hex {3 0ns}
-add_force {/alu32/oper} -radix bin {0110 0ns}
-run 100 ns
-
-add_force {/alu32/a} -radix hex {1 0ns}
-add_force {/alu32/b} -radix hex {3 0ns}
-add_force {/alu32/oper} -radix bin {0111 0ns}
-run 100 ns
-
-add_force {/alu32/a} -radix hex {1 0ns}
-add_force {/alu32/b} -radix hex {3 0ns}
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {9 0ns}
 add_force {/alu32/oper} -radix bin {1100 0ns}
 run 100 ns
 
+
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {9 0ns}
+add_force {/alu32/oper} -radix bin {0010 0ns}
+run 100 ns
+
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {9 0ns}
+add_force {/alu32/oper} -radix bin {0110 0ns}
+run 100 ns
+
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {9 0ns}
+add_force {/alu32/oper} -radix bin {0111 0ns}
+run 100 ns
+
+
+add_force {/alu32/a} -radix hex {80000005 0ns}
+add_force {/alu32/b} -radix hex {80000009 0ns}
+add_force {/alu32/oper} -radix bin {0010 0ns}
+run 100 ns
+
+add_force {/alu32/a} -radix hex {80000005 0ns}
+add_force {/alu32/b} -radix hex {80000009 0ns}
+add_force {/alu32/oper} -radix bin {0110 0ns}
+run 100 ns
+
+add_force {/alu32/a} -radix hex {80000005 0ns}
+add_force {/alu32/b} -radix hex {80000009 0ns}
+add_force {/alu32/oper} -radix bin {0111 0ns}
+run 100 ns
+
+
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {80000001 0ns}
+add_force {/alu32/oper} -radix bin {0010 0ns}
+run 100 ns
+
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {80000001 0ns}
+add_force {/alu32/oper} -radix bin {0110 0ns}
+run 100 ns
+
+add_force {/alu32/a} -radix hex {5 0ns}
+add_force {/alu32/b} -radix hex {80000001 0ns}
+add_force {/alu32/oper} -radix bin {0111 0ns}
+run 100 ns
 
--- alu_one_bit.tcl
+++ alu_one_bit.tcl
@@ -1,89 +1,58 @@
-add_force {/one_bit_alu/a} -radix hex {0 0ns}
-add_force {/one_bit_alu/b} -radix hex {0 0ns}
 add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
 add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
 add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
-add_force {/one_bit_alu/less} -radix hex {0 0ns}
 add_force {/one_bit_alu/oper} -radix hex {0 0ns}
-run 100 ns
-
-add_force {/one_bit_alu/a} -radix hex {1 0ns}
-add_force {/one_bit_alu/b} -radix hex {0 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
-add_force {/one_bit_alu/less} -radix hex {0 0ns}
-add_force {/one_bit_alu/oper} -radix hex {0 0ns}
-run 100 ns
-
 add_force {/one_bit_alu/a} -radix hex {1 0ns}
 add_force {/one_bit_alu/b} -radix hex {1 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
 add_force {/one_bit_alu/less} -radix hex {0 0ns}
-add_force {/one_bit_alu/oper} -radix hex {0 0ns}
 run 100 ns
 
-add_force {/one_bit_alu/a} -radix hex {1 0ns}
-add_force {/one_bit_alu/b} -radix hex {0 0ns}
 add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
 add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
 add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
-add_force {/one_bit_alu/less} -radix hex {0 0ns}
 add_force {/one_bit_alu/oper} -radix hex {1 0ns}
-run 100 ns
-
-add_force {/one_bit_alu/a} -radix hex {1 0ns}
-add_force {/one_bit_alu/b} -radix hex {1 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
-add_force {/one_bit_alu/less} -radix hex {0 0ns}
-add_force {/one_bit_alu/oper} -radix hex {1 0ns}
-run 100 ns
-
 add_force {/one_bit_alu/a} -radix hex {0 0ns}
-add_force {/one_bit_alu/b} -radix hex {1 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
+add_force {/one_bit_alu/b} -radix hex {0 0ns}
 add_force {/one_bit_alu/less} -radix hex {0 0ns}
-add_force {/one_bit_alu/oper} -radix hex {2 0ns}
 run 100 ns
 
-add_force {/one_bit_alu/a} -radix hex {0 0ns}
-add_force {/one_bit_alu/b} -radix hex {1 0ns}
 add_force {/one_bit_alu/a_inv} -radix hex {1 0ns}
 add_force {/one_bit_alu/b_inv} -radix hex {1 0ns}
 add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
+add_force {/one_bit_alu/oper} -radix hex {0 0ns}
+add_force {/one_bit_alu/a} -radix hex {0 0ns}
+add_force {/one_bit_alu/b} -radix hex {0 0ns}
 add_force {/one_bit_alu/less} -radix hex {0 0ns}
+run 100 ns
+
+add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
+add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
+add_force {/one_bit_alu/carry_in} -radix hex {0 0ns}
 add_force {/one_bit_alu/oper} -radix hex {2 0ns}
-run 100 ns
-
 add_force {/one_bit_alu/a} -radix hex {1 0ns}
 add_force {/one_bit_alu/b} -radix hex {1 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {1 0ns}
 add_force {/one_bit_alu/less} -radix hex {0 0ns}
+run 100 ns
+
+add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
+add_force {/one_bit_alu/b_inv} -radix hex {1 0ns}
+add_force {/one_bit_alu/carry_in} -radix hex {1 0ns}
 add_force {/one_bit_alu/oper} -radix hex {2 0ns}
-run 100 ns
-
-add_force {/one_bit_alu/a} -radix hex {1 0ns}
+add_force {/one_bit_alu/a} -radix hex {0 0ns}
 add_force {/one_bit_alu/b} -radix hex {1 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {1 0ns}
-add_force {/one_bit_alu/less} -radix hex {1 0ns}
-add_force {/one_bit_alu/oper} -radix hex {3 0ns}
-run 100 ns
-
-add_force {/one_bit_alu/a} -radix hex {1 0ns}
-add_force {/one_bit_alu/b} -radix hex {1 0ns}
-add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/b_inv} -radix hex {0 0ns}
-add_force {/one_bit_alu/carry_in} -radix hex {1 0ns}
 add_force {/one_bit_alu/less} -radix hex {0 0ns}
-add_force {/one_bit_alu/oper} -radix hex {3 0ns}
 run 100 ns
+
+add_force {/one_bit_alu/a_inv} -radix hex {0 0ns}
+add_force {/one_bit_alu/b_inv} -radix hex {1 0ns}
+add_force {/one_bit_alu/carry_in} -radix hex {1 0ns}
+add_force {/one_bit_alu/oper} -radix hex {3 0ns}
+add_force {/one_bit_alu/a} -radix hex {0 0ns}
+add_force {/one_bit_alu/b} -radix hex {1 0ns}
+add_force {/one_bit_alu/less} -radix hex {0 0ns}
+run 100 ns
+
+
+
+
+
