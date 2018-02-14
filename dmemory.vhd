072fc448c8623bc5ca16483ed7c83cc18c1eb40f
Reno Farnesi <nfarnesi4@gmail.com>
2018-02-14 14:24:47 -0500

    dmemory: change to be bigendian

--- dmemory.vhd
+++ dmemory.vhd
@@ -12,15 +12,15 @@ end entity dmemory;
 
 architecture rtl of dmemory is
 	type byte_mem is array (integer range <>) of std_logic_vector(7 downto 0);
-	signal dmem : byte_mem(0 to 255) := (7 => X"00",
+	signal dmem : byte_mem(0 to 255) := (7 => X"04",
 	                                     6 => X"00",
 	                                     5 => X"00",
-	                                     4 => X"04",
+	                                     4 => X"00",
 	                                     -- 8-11 = 5
-	                                     11 => X"00",
+	                                     11 => X"05",
 	                                     10 => X"00",
 	                                     9  => X"00",
-	                                     8  => X"05",
+	                                     8  => X"00",
 	                                     others => (others => '0'));
 
 	signal addr_3, addr_2, addr_1, addr_0 : unsigned(31 downto 0);
@@ -57,10 +57,10 @@ begin
 	begin
 		if rising_edge(clk) then
 			if mem_read = '0' and mem_write = '1' then
-				dmem(to_integer(addr_3)) <=	write_data(31 downto 24);
-				dmem(to_integer(addr_2)) <=	write_data(23 downto 16);
-				dmem(to_integer(addr_1)) <=	write_data(15 downto 8);
-				dmem(to_integer(addr_0)) <=	write_data( 7 downto 0);
+				dmem(to_integer(addr_0)) <=	write_data(31 downto 24);
+				dmem(to_integer(addr_1)) <=	write_data(23 downto 16);
+				dmem(to_integer(addr_2)) <=	write_data(15 downto 8);
+				dmem(to_integer(addr_3)) <=	write_data( 7 downto 0);
 			end if;
 		end if;
 	end process;
@@ -69,8 +69,8 @@ begin
 	begin
 		if falling_edge(clk) then
 			if mem_read = '1' and mem_write = '0' then
-				read_data <= dmem(to_integer(addr_3)) & dmem(to_integer(addr_2)) &
-			        	     dmem(to_integer(addr_1)) & dmem(to_integer(addr_0));
+				read_data <= dmem(to_integer(addr_0)) & dmem(to_integer(addr_1)) &
+			        	     dmem(to_integer(addr_2)) & dmem(to_integer(addr_3));
 			else
 				read_data <= (others => '0');
 			end if;
