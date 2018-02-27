69e994a73a6b570080930cc0ead68bc7c5e8b195
Reno Farnesi <nfarnesi4@gmail.com>
2018-02-26 20:31:06 -0500

    controller: add tcl script for the controller

---
+++ controller.tcl
@@ -0,0 +1,10 @@
+add_force {/controller/opcode} -radix bin {000000 0ns}
+run 100 ns
+add_force {/controller/opcode} -radix bin {100011 0ns}
+run 100 ns
+add_force {/controller/opcode} -radix bin {101011 0ns}
+run 100 ns
+add_force {/controller/opcode} -radix bin {000100 0ns}
+run 100 ns
+add_force {/controller/opcode} -radix bin {000010 0ns}
+run 100 ns
